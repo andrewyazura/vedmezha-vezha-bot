e57819759668c14fde49f08bad3b4b18ecea55b8
Andrew Yatsura <andrewyazura203@gmail.com>
2022-02-12 17:39:51 +0200

    Add script for creating bundle

---
+++ scripts/bundle.sh
@@ -0,0 +1,10 @@
+rm -rf bundle/
+mkdir bundle/
+
+cp -r src/ bundle/
+cp .env bundle/
+cp .env.example bundle/
+cp app.py bundle/
+cp config.py bundle/
+cp README.md bundle/
+cp requirements.txt bundle/
